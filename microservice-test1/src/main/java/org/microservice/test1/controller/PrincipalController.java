ff15d9db5d540b34771693f65a72aa809b1cedb1
davidillanis <brayanillanis@gmail.com>
2024-08-17 08:41:02 -0500

    version test1

--- microservice-test1/src/main/java/org/microservice/test1/controller/PrincipalController.java
+++ microservice-test1/src/main/java/org/microservice/test1/controller/PrincipalController.java
@@ -1,14 +1,24 @@
 package org.microservice.test1.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/test1/api")
+@RequestMapping("/principal")
 public class PrincipalController {
-    @GetMapping
-    public String hello() {
-        return "hello";
+    @GetMapping(path = "/david")
+    public ResponseEntity<?> hello() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "David");
+        map.put("dateBirth", LocalDate.of(2004, Month.JULY, 11));
+        map.put("country", "Peru");
+        return ResponseEntity.ok(map);
     }
 }
